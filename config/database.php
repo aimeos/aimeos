5ce58c3cf7036136dc5de2eed60822210973233f
Aimeos <aimeos@aimeos.org>
2019-04-29 19:51:17 +0200

    Enforce InnoDB for all Laravel tables

--- config/database.php
+++ config/database.php
@@ -51,7 +51,7 @@ return [
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'strict' => true,
-            'engine' => null,
+            'engine' => 'InnoDB',
         ],
 
         'pgsql' => [
