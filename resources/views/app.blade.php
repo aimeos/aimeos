2c424fac39b9659d90899ec64beb040f7515e29e
Aimeos <aimeos@aimeos.org>
2019-11-05 14:20:29 +0100

    Improved navbar

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,49 +9,36 @@
 
 	<title>Aimeos on Laravel</title>
 
+	@yield('aimeos_styles')
+
 	<link type="text/css" rel="stylesheet" href='https://fonts.googleapis.com/css?family=Roboto:400,300'>
 	<link type="text/css" rel="stylesheet" href="/css/app.css">
 
-	@yield('aimeos_styles')
-
 </head>
 <body>
-	<nav class="navbar navbar-default">
-
-		@yield('aimeos_head')
-
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle Navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-                <a class="navbar-brand-image" href="/">
-                    <img src="http://aimeos.org/fileadmin/template/icons/logo.png" title="Aimeos Logo">
-                </a>
-			</div>
-
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav navbar-right">
-
-					@if (Auth::guest())
-						<li><a href="/login">Login</a></li>
-						<li><a href="/register">Register</a></li>
-					@else
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{
-route('aimeos_shop_account',['site'=>Route::current()->parameter('site','default'),'locale'=>Route::current()->parameter('locale','en'),'currency'=>Route::current()->parameter('currency','EUR')])
-}}" title="Profile">Profile</a></li>
-								<li><form id="logout" action="/logout" method="POST">{{csrf_field()}}</form><a href="javascript: document.getElementById('logout').submit();">Logout</a></li>
-							</ul>
-						</li>
-					@endif
-				</ul>
-			</div>
+	<nav class="navbar navbar-expand-sm navbar-light">
+		<a class="navbar-brand" href="/">
+			<img src="http://aimeos.org/fileadmin/template/icons/logo.png" height="30" title="Aimeos Logo">
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				@if (Auth::guest())
+					<li class="nav-item navbar-text"><a class="nav-link" href="/login">Login</a></li>
+					<li class="nav-item navbar-text"><a class="nav-link" href="/register">Register</a></li>
+				@else
+					<li class="nav-item navbar-text dropdown">
+						<a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="{{ route('aimeos_shop_account',['site'=>Route::current()->parameter('site','default'),'locale'=>Route::current()->parameter('locale','en'),'currency'=>Route::current()->parameter('currency','EUR')]) }}" title="Profile">Profile</a></li>
+							<li><form id="logout" action="/logout" method="POST">{{csrf_field()}}</form><a href="javascript: document.getElementById('logout').submit();">Logout</a></li>
+						</ul>
+					</li>
+				@endif
+			</ul>
+			@yield('aimeos_head')
 		</div>
 	</nav>
     <div class="container">
