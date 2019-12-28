13429645545e77e5d44e504a3924b12dad86bd67
Aimeos <aimeos@aimeos.org>
2019-12-28 16:36:57 +0100

    Avoid mixed HTTP/HTTPS content

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -18,7 +18,7 @@
 <body>
 	<nav class="navbar navbar-expand-sm navbar-light">
 		<a class="navbar-brand" href="/">
-			<img src="http://aimeos.org/fileadmin/template/icons/logo.png" height="30" title="Aimeos Logo">
+			<img src="https://aimeos.org/fileadmin/template/icons/logo.png" height="30" title="Aimeos Logo">
 		</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
