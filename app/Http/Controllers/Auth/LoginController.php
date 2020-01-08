fc1063ac01c7b24973f80d3c64676ce87c8c0fa3
Aimeos <aimeos@aimeos.org>
2020-01-08 16:59:37 +0100

    Redirect to /myaccount URL after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/myaccount';
 
     /**
      * Create a new controller instance.
