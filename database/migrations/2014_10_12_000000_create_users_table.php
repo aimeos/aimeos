a644f0b24cb000c993ce9ce2e322dbfd2397a714
Aimeos <aimeos@aimeos.org>
2020-07-21 15:24:25 +0200

    Enforce InnoDB engine for users table It's required for foreign key constraints by users_* tables

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->engine = 'InnoDB';
         });
     }
 
