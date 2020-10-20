fd94a13ad13f8f62d64783594403a50091616055
Aimeos <aimeos@aimeos.org>
2020-10-20 10:54:24 +0200

    Fixed redirect routes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/profile';
 
     /**
      * Create a new controller instance.
