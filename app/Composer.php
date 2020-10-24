3ca987c1b08003596d9fe6da113e88b05d8203b3
Aimeos <aimeos@aimeos.org>
2020-10-24 15:45:30 +0200

    Support for composer 2.x

--- app/Composer.php
+++ app/Composer.php
@@ -121,10 +121,13 @@ Made with <fg=green>love</> by the Aimeos community. Be a part of it!
 	{
 		$event->getIO()->write( self::$template );
 
+		if( !$event->getIO()->hasAuthentication( 'github.com' ) ) {
+			return;
+		}
+
 		try
 		{
-			$fs = \Composer\Factory::createRemoteFilesystem( $event->getIO(), $event->getComposer()->getConfig() );
-			$fs->getContents( 'github.com', 'https://api.github.com/graphql', false, [
+			$options = [
 				'http' => [
 					'method' => 'POST',
 					'header' => ['Content-Type: application/json'],
@@ -134,9 +137,21 @@ Made with <fg=green>love</> by the Aimeos community. Be a part of it!
 						_3: addStar(input:{clientMutationId:"_3",starrableId:"MDEwOlJlcG9zaXRvcnkyNjg4MTc2NQ=="}){clientMutationId}
 						_4: addStar(input:{clientMutationId:"_4",starrableId:"MDEwOlJlcG9zaXRvcnkyMjIzNTY4OTA="}){clientMutationId}
 						}'
-					] ),
-				],
-			] );
+					] )
+				]
+			];
+			$config = $event->getComposer()->getConfig();
+
+			if( method_exists( '\Composer\Factory', 'createHttpDownloader' ) )
+			{
+				\Composer\Factory::createHttpDownloader( $event->getIO(), $config )
+					->get( 'https://api.github.com/graphql', $options );
+			}
+			else
+			{
+				$fs = \Composer\Factory::createRemoteFilesystem( $event->getIO(), $config )
+					->getContents( 'github.com', 'https://api.github.com/graphql', false, $options );
+			}
 		}
 		catch( \Exception $e ) {}
 	}
