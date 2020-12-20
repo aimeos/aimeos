30507f32567ea0d0d6570bf2ae71753007112e4d
Aimeos <aimeos@aimeos.org>
2020-12-20 12:46:33 +0100

    Redirect to /admin for merchant registration

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -26,4 +26,16 @@ class ResetPasswordController extends Controller
      * @var string
      */
     protected $redirectTo = '/profile';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        if( config( 'app.shop_registration' ) ) {
+            $this->redirectTo = '/admin';
+        }
+    }
 }
