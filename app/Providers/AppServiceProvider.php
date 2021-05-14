2cfa6cc58bb56b560c416ea21bf2106cb43723e0
Aimeos <aimeos@aimeos.org>
2021-05-14 11:52:55 +0200

    Added Aimeos site item to all blade views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        view()->share( 'aimeosSite', app( 'aimeos.context' )->get()->getLocale()->getSiteItem() );
     }
 }
