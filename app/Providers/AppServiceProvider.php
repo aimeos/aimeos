74a1a289fbc0e2c8efd740b9cc63bd5b0b79a0ce
Aimeos <aimeos@aimeos.org>
2021-05-22 17:24:13 +0200

    Postpone adding site item to views

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        view()->share( 'aimeosSite', app( 'aimeos.context' )->get()->getLocale()->getSiteItem() );
+        View::composer('*', function ( $view ) {
+            $view->with( 'aimeossite', app( 'aimeos.context' )->get()->getLocale()->getSiteItem() );
+        });
     }
 }
