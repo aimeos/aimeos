5411513f830dc61b7bd66f6dd8b9efadf38fd0ec
Aimeos <aimeos@aimeos.org>
2022-05-27 11:36:16 +0200

    Improved routes

--- routes/web.php
+++ routes/web.php
@@ -13,23 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+if( env( 'SHOP_MULTILOCALE' ) ) {
+    $locale = ['prefix' => '{locale}', 'where' => ['locale' => '[a-zA-Z]{2}(\_[a-zA-Z]{2})?']];
+}
+
+Route::group($locale ?? [], function() {
+
+    require __DIR__.'/auth.php';
+
+    Route::match(['GET', 'POST'], 'p/{path?}', '\Aimeos\Shop\Controller\PageController@indexAction')
+        ->name('aimeos_page')->where( 'path', '.*' );
+
+    Route::get('/', '\Aimeos\Shop\Controller\CatalogController@homeAction')
+        ->name('aimeos_home');
+});
+
 if( env( 'SHOP_MULTILOCALE' ) )
 {
-    $locale = ['prefix' => '{locale}', 'where' => ['locale' => '[a-zA-Z]{2}(\_[a-zA-Z]{2})?']];
-
     Route::get('/', function () {
         return redirect(airoute('aimeos_home', ['locale' => app()->getLocale()]));
     });
 }
-
-
-Route::group($locale ?? [], function() {
-
-    Route::group(['prefix' => 'auth'], function() {
-        require __DIR__.'/auth.php';
-    });
-
-    Route::match(['GET', 'POST'], '{path?}', '\Aimeos\Shop\Controller\PageController@indexAction')
-        ->name('aimeos_page')->where( 'path', '.*' );
-
-});
