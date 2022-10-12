6cb4238e598ae8d499dde475d799deeac9e4b63c
Aimeos <aimeos@aimeos.org>
2022-10-12 19:15:46 +0200

    Enforce no caching of login page with CSRF token by browser

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -17,7 +18,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function create()
     {
-        return view('auth.login');
+        return Response::view('auth.login')->header( 'Cache-Control', 'no-store, private' );
     }
 
     /**
