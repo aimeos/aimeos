25931c2bb3f3e88a517b9c5af83bdf0c6bf35aa6
Aimeos <aimeos@aimeos.org>
2022-12-19 11:20:16 +0100

    Added account input field for multi-vendor registration again

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,12 +6,21 @@
         <form method="POST" action="{{ airoute('register') }}">
             @csrf
 
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
+            @if(config('app.shop_registration'))
+                <!-- Code -->
+                <div>
+                    <x-label for="code" :value="__('Account')" />
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
+                    <x-input id="code" class="block mt-1 w-full" type="text" name="code" :value="old('code')" required autofocus />
+                </div>
+            @else
+                <!-- Name -->
+                <div>
+                    <x-label for="name" :value="__('Name')" />
+
+                    <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+                </div>
+            @endif
 
             <!-- Email Address -->
             <div class="mt-4">
@@ -25,9 +34,9 @@
                 <x-label for="password" :value="__('Password')" />
 
                 <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
+                    type="password"
+                    name="password"
+                    required autocomplete="new-password" />
             </div>
 
             <!-- Confirm Password -->
@@ -35,8 +44,8 @@
                 <x-label for="password_confirmation" :value="__('Confirm Password')" />
 
                 <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
+                    type="password"
+                    name="password_confirmation" required />
             </div>
 
             <div class="flex items-center justify-end mt-4">
