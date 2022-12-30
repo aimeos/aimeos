c1d4023ad0d56971dc5409c1cba3003ed7a43125
Aimeos <aimeos@aimeos.org>
2022-12-30 09:42:37 +0100

    Enforce HTTPS in production mode

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,6 +30,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if( env('APP_ENV') === 'production' ) {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
+
         Password::defaults(function () {
             $rule = Password::min( 8 );
             return $this->app->isProduction() ? $rule->mixedCase()->uncompromised() : $rule;
