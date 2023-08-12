73578b14adcf090eeec74955d06663a0ae1dd91b
Aimeos <aimeos@aimeos.org>
2023-08-12 10:30:49 +0200

    Fixed route for resetting password

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -2,7 +2,7 @@
     <!-- Validation Errors -->
     <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-    <form method="POST" action="{{ airoute('password.update') }}">
+    <form method="POST" action="{{ airoute('password.store') }}">
         @csrf
 
         <!-- Password Reset Token -->
@@ -11,24 +11,23 @@
         <!-- Email Address -->
         <div>
             <x-label for="email" :value="__('Email')" />
-
             <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
             <x-label for="password" :value="__('Password')" />
-
-            <x-input id="password" class="block mt-1 w-full" type="password" name="password" required />
+            <x-input id="password" class="block mt-1 w-full" type="password" name="password" autocomplete="new-password" required />
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-4">
             <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
             <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
+                type="password"
+                name="password_confirmation"
+                autocomplete="new-password"
+                required />
         </div>
 
         <div class="flex items-center justify-end mt-4">
