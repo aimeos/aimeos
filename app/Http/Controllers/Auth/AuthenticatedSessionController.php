4367d0b001e28143939588a3318d0b4a2e06c3ed
Aimeos <aimeos@aimeos.org>
2023-09-13 18:33:30 +0200

    Save previous URL to redirect to after login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -17,6 +17,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View
     {
+        session()->put('url.intended', url()->previous());
         return view('auth.login');
     }
 
