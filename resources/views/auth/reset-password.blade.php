b43a75fe09caf7291b9767ccb46d3ebe28105be5
Aimeos <aimeos@aimeos.org>
2024-10-22 17:22:36 +0200

    Fixed password resets

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,7 +1,4 @@
 <x-guest-layout>
-    <!-- Validation Errors -->
-    <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
     <form method="POST" action="{{ airoute('password.store') }}">
         @csrf
 
@@ -10,33 +7,33 @@
 
         <!-- Email Address -->
         <div>
-            <x-label for="email" :value="__('Email')" />
-            <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus />
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-label for="password" :value="__('Password')" />
-            <x-input id="password" class="block mt-1 w-full" type="password" name="password" autocomplete="new-password" required />
+            <x-input-label for="password" :value="__('Password')" />
+            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-label for="password_confirmation" :value="__('Confirm Password')" />
-            <x-input id="password_confirmation" class="block mt-1 w-full"
-                type="password"
-                name="password_confirmation"
-                autocomplete="new-password"
-                required />
+            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                                type="password"
+                                name="password_confirmation" required autocomplete="new-password" />
+
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-button>
+            <x-primary-button>
                 {{ __('Reset Password') }}
-            </x-button>
+            </x-primary-button>
         </div>
     </form>
 </x-guest-layout>
